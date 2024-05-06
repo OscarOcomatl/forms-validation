2370baa340950d2bd5c2f34f60ca8febc8e2d180
Oscar Perez <142538269+operezcc@users.noreply.github.com>
2024-05-06 14:08:29 -0600

    Agregando validaciones del formulario de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/providers/login_form_provider.dart';
 // import 'package:flutter/widgets.dart';
 import 'package:productos_app/widgets/widgets.dart';
 import 'package:productos_app/ui/input_decorations.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
    
@@ -22,13 +24,17 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox( height: 10),
                     Text('Login', style: Theme.of(context).textTheme.headlineMedium,),
                     const SizedBox( height: 20),
-                    _LoginForm()
+                    ChangeNotifierProvider(
+                      create: ( _ ) => LoginFormProvider(),
+                      child: _LoginForm(),
+                    ),
+                    // _LoginForm()
                   ],
                 ),
               ),
               const SizedBox(height: 50),
-              Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-              SizedBox(height: 50,)
+              const Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 50,)
             ],
           ),
         ),
@@ -38,23 +44,36 @@ class LoginScreen extends StatelessWidget {
 }
 
 class _LoginForm extends StatelessWidget {
-  const _LoginForm({super.key});
+  //  _LoginForm({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
     return Container(
       child: Form(
         //TODO: Mantener la referencia al KEY
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
           children: [
             TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecorations.authInputDecoration(
+                // String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
                 hintText: 'alguien@gmail.com',
                 labelText: 'Correo electronico',
                 prefixIcon: Icons.alternate_email_rounded
                 ),
+                onChanged: (value) => loginForm.email = value,
+                validator: (value) {
+                String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp =  RegExp(pattern);
+                return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'El valor ingresado no luce como un correo';
+                },
             ),
             const SizedBox(height: 30,),
             TextFormField(
@@ -66,6 +85,12 @@ class _LoginForm extends StatelessWidget {
                 labelText: 'Contraseña',
                 prefixIcon: Icons.lock_rounded
               ),
+              onChanged: (value) => loginForm.password = value,
+              validator: (value) {
+                  return (value != null && value.length >= 6) 
+                  ? null
+                  : 'La contraseña debe ser de 6 caracteres}';
+              },
             ),
             
             const SizedBox(height: 30,),
@@ -74,16 +99,24 @@ class _LoginForm extends StatelessWidget {
               disabledColor: Colors.grey,
               elevation: 0,
               color:Colors.deepPurple,
+              onPressed: loginForm.isLoading ? null : () async {
+                FocusScope.of(context).unfocus();
+                if(!loginForm.isValidForm()) return;
+                loginForm.isLoading = true;
+                Future.delayed(const Duration(seconds: 2));
+                //TODO: Validar si el login es correcto
+                loginForm.isLoading = false;
+                Navigator.pushReplacementNamed(context, 'home');
+              },
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
                 child: Text(
-                  'Ingresar',
-                  style: TextStyle(color: Colors.white),
+                  loginForm.isLoading
+                  ? 'Espere'
+                  : 'Ingresar',
+                  style: const TextStyle(color: Colors.white),
                 ),
               ),
-              onPressed: (){
-                //T
-              }
             )
           ],
         ),
